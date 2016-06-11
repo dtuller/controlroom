6e05def116dd5ee88f8ab57cdf93e64d7e581023
Dan Tuller <dtuller@gmail.com>
2016-06-10 21:52:06 -0500

    Removing scenes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160521215755) do
+ActiveRecord::Schema.define(version: 20160611014148) do
 
   create_table "lightbulbs", force: :cascade do |t|
     t.integer  "user_id"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20160521215755) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
